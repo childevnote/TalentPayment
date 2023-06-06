40d8967763cd1ab66c1325494a57b0a742cd3d63
hajun <audgkwns44@gmail.com>
2023-06-06 18:51:50 +0900

    [Setting]Enable auto formatter

--- TalentPayapp/App.js
+++ TalentPayapp/App.js
@@ -1,26 +1,36 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const HomeScreen = () => {
-  return <View><Text>Home</Text></View>
-}
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
 export default function App() {
-  return <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
