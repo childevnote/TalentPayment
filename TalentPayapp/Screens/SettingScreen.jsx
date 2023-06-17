de23d392bb79ec97760a70b3c8e76da55ca8c863
childevnote <daniel011125@gmail.com>
2023-06-18 01:57:22 +0900

    [Create] Select Theme

--- TalentPayapp/Screens/SettingScreen.jsx
+++ TalentPayapp/Screens/SettingScreen.jsx
@@ -1,5 +1,29 @@
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import SelectTheme from "../Components/SelectTheme";
 
 export default function SettingScreen() {
-  return <View></View>;
+  return (<View style={styles.wrapper}>
+    <View style={styles.logoContainer}>
+      <Image source={require('../assets/logo_x.png')} style={styles.logo} />
+    </View>
+    <SelectTheme />
+  </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    height: "100%",
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    left: 20,
+  },
+  logo: {
+    width: 150,
+    height: 60,
+    resizeMode: 'contain',
+  },
+});
