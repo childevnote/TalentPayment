3e6b2b5b4ffa8e5a74c5332adf5f98045ea696e4
hajun <audgkwns44@gmail.com>
2023-07-19 21:35:51 +0900

    [QR_Generator][Update]data reading

--- qrGenerator/data_processor.py
+++ qrGenerator/data_processor.py
@@ -1,5 +1,24 @@
 def main():
-    
+    filepath = "./data/"
+    # filename = "participants.txt"
+    filename = "test.txt"
+
+    with open(f"{filepath}{filename}", mode="+r") as fs:
+        curIdx = 0
+        for line in fs:
+            if curIdx == 0:
+                curIdx += 1
+                continue
+            else:
+                newData = line.split("\t")
+
+                if newData[3] == "TRUE":
+                    print(f"{newData[1]}_선생님")
+                else:
+                    print(f"{newData[1]}_{newData[4]}")
+
+                curIdx += 1
+
     return True
 
 if __name__ == "__main__" :
