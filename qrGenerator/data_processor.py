b38f3364bf2a85d5c78f40f71596779112f6f4d2
hajun <audgkwns44@gmail.com>
2023-07-19 22:08:30 +0900

    [QR_Generator][Update]complete to implement data processor

--- qrGenerator/data_processor.py
+++ qrGenerator/data_processor.py
@@ -1,12 +1,20 @@
 import qrcode
 import random
+import os
 
 def main():
     filepath = "./data/"
-    # filename = "participants.txt"
-    filename = "test.txt"
+    filename = "participants.txt"
+    # filename = "test.txt"
 
     qrFilename = ""
+    qrNames = []
+
+    writeFilename = "./list.txt"
+    if os.path.exists(writeFilename):
+        os.remove(writeFilename)
+    
+    duplicateChecker = ["3890"]
 
     with open(f"{filepath}{filename}", mode="+r") as fs:
         curIdx = 0
@@ -21,15 +29,33 @@ def main():
                     qrFilename = f"{newData[1]}_선생님"
                 else:
                     if newData[4][-1:] == "\n":
-                        qrFilename = f"{newData[1]}_{newData[4][:-1]}.png"
+                        qrFilename = f"{newData[1]}_{newData[4][:-1]}"
                     else:
-                        qrFilename = f"{newData[1]}_{newData[4]}.png"
+                        qrFilename = f"{newData[1]}_{newData[4]}"
 
                 curIdx += 1
             
-            newId = int(random.random()*1000)+1000*random.randint(1, 9)
+            qrNames.append(qrFilename)
+            
+            newId = 1000
+
+            while True:
+                newId = int(random.random()*1000)+1000*random.randint(1, 9)
+                if f"{newId}" in duplicateChecker:
+                    continue
+                else:
+                    duplicateChecker.append(f"{newId}")
+                    break
+
             newImg = qrcode.make(f"{newId}")
             newImg.save(f"images/{qrFilename}_{newId}.png")
+        
+        fs.close()
+    
+    with open(writeFilename, "+w") as fs:
+        for data in qrNames:
+            fs.write(f"{data}\n")
+        fs.close()
 
     return True
 
